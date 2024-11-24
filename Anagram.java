2f8a2e617d78b4dad23fd9830e6b8f5db9b3915c
Shakedflam <shakedflam@gmail.com>
2024-11-24 18:18:45 +0200

    Anagram first upload

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,83 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String firstStr=preProcess(str1);
+		String secondStr=preProcess(str2);
+		int length=firstStr.length();
+		if (firstStr.length()!=secondStr.length()){
+			return false;
+		}
+		for (int i=0;i<length;i++){
+			int noMatch=0;
+			char atI=firstStr.charAt(i);
+			for (int j=0; j<length;j++){
+				char atJ=secondStr.charAt(j);
+				if (atI!=atJ){
+					noMatch++;
+				}else{
+					break;
+				}
+				if (noMatch==length){
+					return false;
+				}
+			}
+
+		}
+		for (int i=0; i<length;i++){
+			int noMatch=0;
+			char atI=secondStr.charAt(i);
+			for (int j=0;j<length;j++){
+				char atJ=firstStr.charAt(j);
+				if (atJ!=atI){
+					noMatch++;
+				}else{
+					break;
+				}
+				if (noMatch==length){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String lowerCases= str.toLowerCase();
+		String newString="";
+		for (int i=0;i<str.length();i++){
+			if(((lowerCases.charAt(i)>=97 && lowerCases.length()<=122) || (lowerCases.charAt(i)>=48 && lowerCases.charAt(i)<=57)) ){
+				newString="" +newString+lowerCases.charAt(i);
+			}else{
+
+			}
+		}
+		return newString;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String randomAnagrma="";
+		String str1=str;
+		int length=str.length();
+		while (length>0){
+			int randomNumber=(int)(Math.random()*str1.length());
+			randomAnagrma+=str1.charAt(randomNumber);
+			String leftPart="";
+			String rightPart="";
+			for (int i=0;i<randomNumber;i++){
+				leftPart+=str1.charAt(i);
+			}
+			for (int j=randomNumber+1;j<length;j++){
+				rightPart+=str1.charAt(j);
+			}
+			str1=leftPart+rightPart;
+			length--;
+		}
+
+		return randomAnagrma;
 	}
 }
