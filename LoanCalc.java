ccc4af7afe0c0f243ed3d9747060dda0a24f06c8
Shakedflam <shakedflam@gmail.com>
2024-11-24 12:06:43 +0200

    LoanCalca upload

--- LoanCalc.java
+++ LoanCalc.java
@@ -29,11 +29,14 @@ public class LoanCalc {
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
 		double loanAmount=loan;
-		double interestRate=rate;
-		double payingPeriods=n;
+		double interestRate=(rate/100)+1;;
 		double periodPayment=payment;
+		for (int i=0;i<n;i++){
+			loanAmount=(loanAmount-periodPayment)*interestRate;
+		}
 		
-		return 0;
+		
+		return loanAmount;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -42,8 +45,28 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		iterationCounter=-1;
+		double loanAmount=loan;
+		double interestRate=(rate/100)+1;
+		double payingPeriods=n;
+		double periodPayment=loanAmount/payingPeriods;
+		while (loanAmount>epsilon){
+			iterationCounter++;
+			loanAmount=loan;
+			for (int i=0; i<n;i++){
+				loanAmount-=periodPayment;
+				loanAmount*=interestRate;
+			}
+			if (loanAmount>epsilon){
+				periodPayment+=epsilon;
+			}else{
+				return periodPayment;
+			}
+			if (periodPayment>loan){
+			break;
+			}
+		}
+		return periodPayment;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -52,7 +75,24 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		iterationCounter=0;
+		double highAmount=loan;
+		double lowAmount=loan/n;
+		double periodPayment=(highAmount+lowAmount)/(double)2.0;
+		while ((highAmount-lowAmount)>epsilon){
+			iterationCounter++;
+			double balance=endBalance(loan, rate, n, periodPayment);
+			if (balance*endBalance(loan, rate, n, lowAmount)>0){
+				lowAmount=periodPayment;
+			}else{
+				highAmount=periodPayment;
+			}
+			periodPayment=(highAmount+lowAmount)/2.0;
+			if (highAmount==lowAmount) {
+				break;
+			}
+			
+        }
+		return periodPayment;
     }
 }
